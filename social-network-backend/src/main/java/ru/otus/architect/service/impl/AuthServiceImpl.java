ef5b39c3b4681f354eb23c48260a91af4dd0c075
andrey <andrash00@gmail.com>
2021-01-31 22:13:07 +0300

    удалены импорты

--- social-network-backend/src/main/java/ru/otus/architect/service/impl/AuthServiceImpl.java
+++ social-network-backend/src/main/java/ru/otus/architect/service/impl/AuthServiceImpl.java
@@ -1,8 +1,6 @@
 package ru.otus.architect.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
